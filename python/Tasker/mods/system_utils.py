7e2a040eebaa14e659509ba93bd0839f259b9221
Prakash Rao Vaddina - Windows <prakash.vaddina@gmail.com>
2019-11-09 23:20:25 +0100

    Update system utils module

--- python/Tasker/mods/system_utils.py
+++ python/Tasker/mods/system_utils.py
@@ -7,6 +7,8 @@ import interfaces
 
 from subprocess import call
 import subprocess
+import sys
+import os
 
 ###############################################################################
 #
@@ -16,19 +18,53 @@ import subprocess
 
 class SysExec(interfaces.ISingleTask):
     def __init__(self, dictTask):
-        self.__cmd = dictTask["Cmd"]
-        self.__args = ""
-        if "Args" in dictTask:
-          self.__args = dictTask["Args"]
+        self.__cmds = dictTask["AllCmds"]
         self.__name = dictTask["Name"]
 
     def GetInteractiveName(self):
         return self.__name
 
     def Execute(self):
-        finalCommand = self.__cmd + " " + self.__args
-        cmdList = finalCommand.split(" ")
+        execCmd = ""
+        for cmd in self.__cmds:
+          execCmd += cmd
+          execCmd += "; "
+        print("Running the command: " + execCmd )
+
+        ret = subprocess.call(execCmd, shell=True)
+'''
+        ####################################################################
+        # Works but not recommended
+        ####################################################################
+        ret = os.system(execCmd)
+'''
+
+'''
+        ####################################################################
+        # Returns output only after the process has completed !!!
+        ####################################################################
+        process = subprocess.Popen(execCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+        stdout, stderr = process.communicate()
+        return stdout.strip()
+'''
+'''
+        ####################################################################
+        # Blocks (nearly 100% of the time) when no output to read
+        ####################################################################
+        process = subprocess.Popen(execCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+        while True:
+          line = process.stdout.readline()
+          if not line: break
+          else:
+            print(line)
+'''
+'''
+        ####################################################################
+        # Blocks (nearly 100% of the time) when no output to read
+        ####################################################################
+        process = subprocess.Popen(execCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+        cmdList = execCmd.split(" ")
         cmdList = [idx for idx in cmdList if len(idx)>0]
-        print("Running the command: " + finalCommand)
+        print(cmdList)
         console_op = subprocess.run(cmdList)
-    
+'''
